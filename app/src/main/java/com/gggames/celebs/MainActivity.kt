3c5fdb4b7974b3ba205ec9f34fd1d4a78e490aa0
giladgotman <gg@idagio.com>
2020-04-04 10:37:29 +0200

    first-setup

--- app/src/main/java/com/gggames/celebs/MainActivity.kt
+++ app/src/main/java/com/gggames/celebs/MainActivity.kt
@@ -1,10 +1,11 @@
-package celebs.gggames.com
+package com.gggames.celebs
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import celebs.gggames.com.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
