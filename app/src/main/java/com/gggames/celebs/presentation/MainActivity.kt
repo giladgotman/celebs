d1659b595b9bb0ac9e1475e6af99d58506220feb
Gilad Gotman <giladgot@gmail.com>
2020-04-04 11:45:29 +0200

    change fab icon

--- app/src/main/java/com/gggames/celebs/presentation/MainActivity.kt
+++ app/src/main/java/com/gggames/celebs/presentation/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        createGameFab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
