a8542cbd5ec30120092f9d67b78151df8b684c30
giladgotman <gg@idagio.com>
2020-06-21 14:06:32 +0200

    implement presenter partially

--- app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverPresenter.kt
+++ app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverPresenter.kt
@@ -1,31 +1,86 @@
 package com.gggames.celebs.presentation.endgame
 
 import com.gggames.celebs.features.games.data.GamesRepository
-import com.gggames.celebs.presentation.endgame.GameOverScreenContract.State
-import com.gggames.celebs.presentation.endgame.GameOverScreenContract.UiEvent
+import com.gggames.celebs.presentation.endgame.GameOverScreenContract.*
+import com.gggames.celebs.presentation.endgame.GameOverScreenContract.Result.GameResult
+import com.idagio.app.core.utils.rx.scheduler.BaseSchedulerProvider
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Observable.just
+import io.reactivex.Observable.merge
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 import timber.log.Timber
 import javax.inject.Inject
 
 class GameOverPresenter @Inject constructor(
-    val gamesRepository: GamesRepository
-) {
+    val gamesRepository: GamesRepository,
+    val scheduler: BaseSchedulerProvider
+) : Presenter{
 
     private val _states = PublishSubject.create<State>()
-    val states: Observable<State> = _states
+    override val states: Observable<State> = _states
+    private val disposables = CompositeDisposable()
 
-    fun bind(
+    private val _triggers = PublishSubject.create<Trigger>()
+    override val triggers: Observable<Trigger> = _triggers
+
+    override fun bind(
         events: Observable<UiEvent>,
         gameId: String
     ) {
         Timber.w("ggg bind, gameId: $gameId")
+        val results = events.publish {
+            merge(
+                handleUiEvent(it),
+                getCurrentGame()
+            )
+        }
 
+        val states = results.scan(State.initialValue, reduce())
 
+        states.distinctUntilChanged()
+            .compose(scheduler.applyDefault())
+            .subscribe { _states.onNext(it) }
+            .let { disposables.add(it) }
+
+        results
+            .distinctUntilChanged()
+            .compose(scheduler.applyDefault())
+            .subscribe {
+                val trigger = when (it) {
+                    is GameResult -> null
+                    is Result.GameCleared -> Trigger.NavigateToGames
+                }
+                if (trigger != null) {
+                    _triggers.onNext(trigger)
+                }
+            }.let { disposables.add(it) }
     }
 
-    fun unBind() {
+    private fun reduce() = { previous: State, result: Result ->
+        when (result) {
+            is GameResult -> previous.copy(
+                winningTeam = result.game.winningTeam?.name ?: "",
+                teams = result.game.teams.sortedBy { it.score })
+            Result.GameCleared -> previous
+        }
+    }
 
+    private fun handleUiEvent(events: Observable<UiEvent>): Observable<Result> =
+        events.filter { it is UiEvent.PressedFinish }
+            .flatMap { finishGame().andThen(just(Result.GameCleared)) }
+
+
+    private fun finishGame(): Completable =
+        Completable.complete()
+
+    private fun getCurrentGame(): Observable<GameResult> = Observable.fromCallable {
+        gamesRepository.currentGame!!
+    }.map { GameResult(it) }
+
+    override fun unBind() {
+        disposables.clear()
     }
 
 }
--- app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverScreenContract.kt
+++ app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverScreenContract.kt
@@ -3,22 +3,39 @@ package com.gggames.celebs.presentation.endgame
 import com.gggames.celebs.model.Card
 import com.gggames.celebs.model.Game
 import com.gggames.celebs.model.Team
+import io.reactivex.Observable
 
 interface GameOverScreenContract {
 
     data class State(
-        val winningTeam: String,
-        val teams: List<Team>,
-        val cards: List<Card>
-    )
+        val winningTeam: String = "",
+        val teams: List<Team> = emptyList(),
+        val cards: List<Card> = emptyList()
+    ) {
+        companion object {
+            val initialValue = State()
+        }
+    }
+
+    sealed class Trigger {
+        object NavigateToGames : Trigger()
+    }
 
     sealed class Result {
         data class GameResult(val game: Game): Result()
         data class CardsResult(val cards: List<Card>)
+        object GameCleared : Result()
     }
 
     sealed class UiEvent{
         object PressedFinish : UiEvent()
+    }
+
+    interface Presenter {
+        fun bind(events: Observable<UiEvent>, gameId: String)
+        fun unBind()
+        val states: Observable<State>
+        val triggers: Observable<Trigger>
     }
 
 }
