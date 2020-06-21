14da74a451bcef9be7a7d800451a30ea7de95152
giladgotman <gg@idagio.com>
2020-06-21 15:18:59 +0200

    add team adapter

--- app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverFragment.kt
+++ app/src/main/java/com/gggames/celebs/presentation/endgame/GameOverFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.*
 import com.gggames.celebs.R
+import com.gggames.celebs.model.Team
 import com.gggames.celebs.presentation.di.ViewComponent
 import com.gggames.celebs.presentation.di.createViewComponent
 import com.gggames.celebs.presentation.endgame.GameOverScreenContract.Trigger
@@ -14,7 +16,7 @@ import com.gggames.celebs.presentation.endgame.GameOverScreenContract.UiEvent.Pr
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.fragment_game_over.*
-import timber.log.Timber
+import kotlinx.android.synthetic.main.team_score_layout.view.*
 import javax.inject.Inject
 
 class GameOverFragment : Fragment() {
@@ -22,8 +24,9 @@ class GameOverFragment : Fragment() {
     @Inject
     lateinit var presenter: GameOverScreenContract.Presenter
     private lateinit var viewComponent: ViewComponent
+    private val teamsAndCardsAdapter = TeamsAdapter()
 
-    private val events = PublishSubject.create<GameOverScreenContract.UiEvent>()
+        private val events = PublishSubject.create<GameOverScreenContract.UiEvent>()
     private val disposables = CompositeDisposable()
 
 
@@ -48,7 +51,16 @@ class GameOverFragment : Fragment() {
         finishButton.setOnClickListener {
             events.onNext(PressedFinish)
         }
+
+        setupRecyclerView()
         presenter.bind(events, gameId!!)
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManager = LinearLayoutManager(this.context)
+        teamsAndCardsRv.layoutManager = layoutManager
+        teamsAndCardsRv.itemAnimator = DefaultItemAnimator()
+        teamsAndCardsRv.adapter = teamsAndCardsAdapter
     }
 
     private fun trigger(trigger: Trigger) {
@@ -63,12 +75,53 @@ class GameOverFragment : Fragment() {
 
     private fun render(state: GameOverScreenContract.State) {
         subtitle.text = getString(R.string.game_over_subtitle, state.winningTeam)
-        Timber.w("ggg render cards: ${state.cards.size}")
+        teamsAndCardsAdapter.submitList(state.teams)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         disposables.clear()
         presenter.unBind()
+    }
+}
+
+class TeamsAdapter : ListAdapter<Team, TeamsAdapter.TeamViewHolder>(TeamDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        TeamViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.team_score_layout, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class TeamViewHolder(itemView: View)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(item: Team) = with(itemView) {
+            teamName.text = item.name
+            score.text = item.score.toString()
+        }
+
+        override fun onClick(v: View?) {
+
+        }
+    }
+
+    private class TeamDiffUtil : DiffUtil.ItemCallback<Team>() {
+        override fun areItemsTheSame(
+            oldItem: Team,
+            newItem: Team
+        ) = oldItem.name == newItem.name
+
+        override fun areContentsTheSame(
+            oldItem: Team,
+            newItem: Team
+        ) = oldItem == newItem
     }
 }
