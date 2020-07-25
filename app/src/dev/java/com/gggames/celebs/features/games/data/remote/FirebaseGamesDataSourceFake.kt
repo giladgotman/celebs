91c3472cb4fd3b4afec5930bcd8a9f390cedb48d
giladgotman <gg@idagio.com>
2020-07-25 13:35:01 +0200

    add FirebaseGamesDataSourceFake tests

--- app/src/dev/java/com/gggames/celebs/features/games/data/remote/FirebaseGamesDataSourceFake.kt
+++ app/src/dev/java/com/gggames/celebs/features/games/data/remote/FirebaseGamesDataSourceFake.kt
@@ -1,30 +1,44 @@
 package com.gggames.celebs.features.games.data.remote
 
-import com.gggames.celebs.common.factory.createGame
 import com.gggames.celebs.features.games.data.GamesDataSource
 import com.gggames.celebs.model.Game
 import com.gggames.celebs.model.GameState
 import io.reactivex.Completable
-import io.reactivex.Completable.complete
+import io.reactivex.Completable.fromCallable
 import io.reactivex.Observable
 import io.reactivex.Observable.just
+import io.reactivex.Observable.merge
 import io.reactivex.Single
+import io.reactivex.subjects.PublishSubject
 import timber.log.Timber
 import javax.inject.Inject
 
-class FirebaseGamesDataSourceFake @Inject constructor(): GamesDataSource{
+class FirebaseGamesDataSourceFake @Inject constructor() : GamesDataSource {
+
+    var games = mutableListOf<Game>()
+    val gamesSubject = PublishSubject.create<Game>()
+
     override fun getGames(gameIds: List<String>, states: List<GameState>): Single<List<Game>> {
         Timber.w("ggg getGames: size: ${gameIds.size}")
-        return Single.just(emptyList())
+        return Single.just(games.filter { it.id in gameIds && it.state in states })
     }
 
-    override fun setGame(game: Game): Completable {
-        Timber.w("ggg setGame: id: ${game.id}")
-        return complete()
-    }
+    override fun setGame(game: Game): Completable =
+        fromCallable {
+            Timber.w("ggg setGame: id: ${game.id}")
+            if (games.contains(game)) {
+                games[games.indexOf(game)] = game
+            } else {
+                games.add(game)
+            }
+            gamesSubject.onNext(game)
+        }
 
     override fun observeGame(gameId: String): Observable<Game> {
         Timber.w("ggg observeGame, id: $gameId")
-        return just(createGame(id = gameId))
+        val first = games.find { it.id == gameId }?.let {
+            just(it)
+        } ?: Observable.empty<Game>()
+        return merge (gamesSubject.filter { it.id == gameId }, first)
     }
 }
