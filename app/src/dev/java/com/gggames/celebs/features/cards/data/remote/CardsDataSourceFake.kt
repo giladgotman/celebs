837850e561d110f0eb917f07dbb962600c39fe9c
giladgotman <gg@idagio.com>
2020-08-02 14:46:22 +0200

    add fake cards

--- app/src/dev/java/com/gggames/celebs/features/cards/data/remote/CardsDataSourceFake.kt
+++ app/src/dev/java/com/gggames/celebs/features/cards/data/remote/CardsDataSourceFake.kt
@@ -2,7 +2,6 @@ package com.gggames.celebs.features.cards.data.remote
 
 import com.gggames.celebs.features.cards.data.CardsDataSource
 import com.gggames.celebs.model.Card
-import com.gggames.celebs.model.remote.CardRaw
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Observable.just
@@ -28,7 +27,7 @@ class CardsDataSourceFake @Inject constructor() : CardsDataSource {
             val updatedList = this.cards
             cards.forEachIndexed { i, card ->
                 val cardWithId = card.copy(id= "id_$i")
-                updatedList.add(cardWithId)
+                updatedList.add(cardWithId.addFakeUrls())
             }
 
             this.cards = updatedList
@@ -50,14 +49,11 @@ class CardsDataSourceFake @Inject constructor() : CardsDataSource {
         just(cards).flatMapIterable { it }
             .flatMapCompletable { update(it) }
 
-    private fun getFakeCards(cards: List<CardRaw>): List<CardRaw> =
-        cards.map {
-            it.copy(
-                videoUrl1 = "https://drive.google.com/uc?export=download&id=194rl8msLR47b8No3-uuI-AmLre2wgoC9",
-                videoUrl2 = "https://drive.google.com/uc?export=download&id=147xu8GaVe25o3LhJ6xNcElqeEEHD6_vW",
-                videoUrl3 = "https://drive.google.com/uc?export=download&id=1CGIg6YgKin7m-QmHvyQ03omj6yEvWFRG",
-                videoUrlFull = "https://drive.google.com/uc?export=download&id=1k-6jLFqi7YO_QgeCfA_ubU22_vLY-2AO"
-
-            )
-        }
+    private fun Card.addFakeUrls(): Card =
+        this.copy(
+            videoUrl1 = "https://drive.google.com/uc?export=download&id=194rl8msLR47b8No3-uuI-AmLre2wgoC9",
+            videoUrl2 = "https://drive.google.com/uc?export=download&id=147xu8GaVe25o3LhJ6xNcElqeEEHD6_vW",
+            videoUrl3 = "https://drive.google.com/uc?export=download&id=1CGIg6YgKin7m-QmHvyQ03omj6yEvWFRG",
+            videoUrlFull = "https://drive.google.com/uc?export=download&id=1k-6jLFqi7YO_QgeCfA_ubU22_vLY-2AO"
+        )
 }
