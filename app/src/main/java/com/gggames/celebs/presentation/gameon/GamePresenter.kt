4627d813d8732bd3d3fb607e2b65695a562bea57
giladgotman <gg@idagio.com>
2020-08-24 19:06:47 +0200

    set 60 sec turn time

--- app/src/main/java/com/gggames/celebs/presentation/gameon/GamePresenter.kt
+++ app/src/main/java/com/gggames/celebs/presentation/gameon/GamePresenter.kt
@@ -22,8 +22,8 @@ import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
 import javax.inject.Inject
 
-//const val TURN_TIME_MILLIS = 60000L
-const val TURN_TIME_MILLIS = 10000L
+const val TURN_TIME_MILLIS = 60000L
+//const val TURN_TIME_MILLIS = 10000L
 
 class GamePresenter @Inject constructor(
     private val playersObservable: ObservePlayers,
