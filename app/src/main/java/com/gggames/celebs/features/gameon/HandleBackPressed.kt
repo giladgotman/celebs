d036ec1dd3eb3ec038e6b9e3d82783b40562edc5
giladgotman <gg@idagio.com>
2020-09-24 11:51:16 +0200

    pause turn when confirmation dialog is showed

--- app/src/main/java/com/gggames/celebs/features/gameon/HandleBackPressed.kt
+++ app/src/main/java/com/gggames/celebs/features/gameon/HandleBackPressed.kt
@@ -1,21 +1,28 @@
 package com.gggames.celebs.features.gameon
 
 import com.gggames.celebs.core.Authenticator
+import com.gggames.celebs.features.games.domain.SetGame
 import com.gggames.celebs.model.Game
+import com.gggames.celebs.model.TurnState
+import com.gggames.celebs.model.setTurnState
 import com.gggames.celebs.presentation.gameon.GameScreenContract.Result.BackPressedResult
 import io.reactivex.Observable
 import io.reactivex.Observable.just
 import javax.inject.Inject
 
 class HandleBackPressed @Inject constructor(
-    private val authenticator: Authenticator
+    private val authenticator: Authenticator,
+    private val setGame: SetGame
 ) {
-    operator fun invoke(game: Game): Observable<BackPressedResult> =
+    operator fun invoke(game: Game): Observable<out BackPressedResult> =
         if (authenticator.isMyselfActivePlayerBlocking(game)) {
-            just(
-                BackPressedResult.ShowLeaveGameConfirmation(true),
-                BackPressedResult.ShowLeaveGameConfirmation(false)
-            )
+            setGame(game.setTurnState(TurnState.Paused))
+                .andThen(
+                    just(
+                        BackPressedResult.ShowLeaveGameConfirmation(true),
+                        BackPressedResult.ShowLeaveGameConfirmation(false)
+                    )
+                )
         } else {
             just(
                 BackPressedResult.NavigateToGames(true),
