c5c800b1c56192bbea363d92a39eb387b6b652db
giladgotman <gg@idagio.com>
2020-09-27 17:39:17 +0200

    remove unused code

--- app/src/main/java/com/gggames/celebs/presentation/gameon/GamePresenterMVI.kt
+++ app/src/main/java/com/gggames/celebs/presentation/gameon/GamePresenterMVI.kt
@@ -1,13 +1,10 @@
 package com.gggames.celebs.presentation.gameon
 
 import com.gggames.celebs.core.Authenticator
-import com.gggames.celebs.features.cards.data.CardsRepository
 import com.gggames.celebs.features.cards.domain.ObserveAllCards
 import com.gggames.celebs.features.gameon.*
 import com.gggames.celebs.features.games.data.GamesRepository
 import com.gggames.celebs.features.games.domain.ObserveGame
-import com.gggames.celebs.features.games.domain.SetGame
-import com.gggames.celebs.features.players.domain.LeaveGame
 import com.gggames.celebs.features.players.domain.ObservePlayers
 import com.gggames.celebs.model.*
 import com.gggames.celebs.presentation.gameon.GameScreenContract.*
@@ -29,10 +26,8 @@ import javax.inject.Inject
 class GamePresenterMVI @Inject constructor(
     private val playersObservable: ObservePlayers,
     private val cardsObservable: ObserveAllCards,
-    private val updateGame: SetGame,
     private val observeGame: ObserveGame,
     private val authenticator: Authenticator,
-    private val cardsRepository: CardsRepository,
     private val gamesRepository: GamesRepository,
     private val handleNextCard: HandleNextCard,
     private val startGame: StartGame,
@@ -43,7 +38,6 @@ class GamePresenterMVI @Inject constructor(
     private val handleBackPressed: HandleBackPressed,
     private val endTurn: EndTurn,
     private val flipLastCard: FlipLastCard,
-    private val leaveGame: LeaveGame,
     private val audioPlayer: AudioPlayer,
     private val schedulerProvider: BaseSchedulerProvider
 ) {
@@ -56,10 +50,6 @@ class GamePresenterMVI @Inject constructor(
         get() = gamesRepository.currentGame!!
 
     private var lastGame: Game? = null
-    private val roundState: RoundState
-        get() = game.gameInfo.round.state
-
-    private var cardsFoundInTurn = mutableListOf<Card>()
 
     private val _states = PublishSubject.create<State>()
     val states: Observable<State> = _states
