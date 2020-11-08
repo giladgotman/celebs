f977b5dbabbf9c3ff02282f4d20b9be611f2eb49
giladgotman <gg@idagio.com>
2020-11-08 11:35:07 +0100

    add WelcomeActivity.kt

---
+++ app/src/main/java/com/gggames/hourglass/presentation/onboarding/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.gggames.hourglass.presentation.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gggames.hourglass.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
