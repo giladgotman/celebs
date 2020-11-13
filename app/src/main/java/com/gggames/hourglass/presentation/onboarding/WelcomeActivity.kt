a90e8851bbc7760e56cf7e16c7101ffebb89c50c
giladgotman <gg@idagio.com>
2020-11-13 11:44:24 +0100

    remove transition from WelcomeActivity.kt to signup

--- app/src/main/java/com/gggames/hourglass/presentation/onboarding/WelcomeActivity.kt
+++ app/src/main/java/com/gggames/hourglass/presentation/onboarding/WelcomeActivity.kt
@@ -9,9 +9,6 @@ import com.gggames.hourglass.R
 import com.gggames.hourglass.core.CelebsApplication
 import com.gggames.hourglass.presentation.MainActivity
 import com.gggames.hourglass.presentation.login.SignupActivity
-import com.gggames.hourglass.utils.transitions.TRANSITION_FADE
-import com.gggames.hourglass.utils.transitions.setupWindowSlideAnimations
-import com.gggames.hourglass.utils.transitions.startActivityAnimated
 import kotlinx.android.synthetic.main.activity_welcome.*
 import javax.inject.Inject
 
@@ -25,10 +22,9 @@ class WelcomeActivity : AppCompatActivity(), WelcomeContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        setupWindowSlideAnimations()
-
         get_started_button.setOnClickListener {
-            startActivityAnimated(SignupActivity.createIntent(this), TRANSITION_FADE)
+            SignupActivity.start(this)
+            finish()
         }
         initializeCarouselViewPager()
         presenter.bind(this)
