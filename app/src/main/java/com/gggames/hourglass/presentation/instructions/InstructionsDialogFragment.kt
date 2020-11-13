0b0f34bece1123b793bb2eed595ab133c1998ea2
giladgotman <gg@idagio.com>
2020-11-13 14:26:39 +0100

    add InstructionsDialogFragment.kt

--- app/src/main/java/com/gggames/hourglass/presentation/instructions/InstructionsDialogFragment.kt
+++ app/src/main/java/com/gggames/hourglass/presentation/instructions/InstructionsDialogFragment.kt
@@ -1,16 +1,18 @@
 package com.gggames.hourglass.presentation.instructions
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.gggames.hourglass.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class InstructionsDialogFragment : BottomSheetDialogFragment() {
-
 
     fun show(activity: AppCompatActivity) {
         show(activity.supportFragmentManager, this.javaClass.simpleName)
@@ -22,4 +24,18 @@ class InstructionsDialogFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_instructions, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {
+            val bottomSheet = dialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+            val behavior: BottomSheetBehavior<*> = BottomSheetBehavior.from(bottomSheet!!)
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        return dialog
+
+    }
 }
