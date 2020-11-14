b6485caa85451328d799ed53ad09d7220e4c3967
giladgotman <gg@idagio.com>
2020-11-14 11:44:09 +0100

    fix issue with scrollView within the bottomSheet. set the bottom sheet to expanded and with a fixed peek height.

--- app/src/main/java/com/gggames/hourglass/presentation/instructions/InstructionsDialogFragment.kt
+++ app/src/main/java/com/gggames/hourglass/presentation/instructions/InstructionsDialogFragment.kt
@@ -1,6 +1,7 @@
 package com.gggames.hourglass.presentation.instructions
 
 import android.app.Dialog
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,6 @@ import com.gggames.hourglass.presentation.onboarding.WelcomePagerCarouselAdapter
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_instructions.*
-
 
 class InstructionsDialogFragment : BottomSheetDialogFragment() {
 
@@ -28,9 +28,7 @@ class InstructionsDialogFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initializeCarouselViewPager()
-
     }
 
     private fun initializeCarouselViewPager() {
@@ -47,12 +45,18 @@ class InstructionsDialogFragment : BottomSheetDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
+        setupBottomSheet(dialog)
+        return dialog
+
+    }
+
+    private fun setupBottomSheet(dialog: Dialog) {
         dialog.setOnShowListener {
             val bottomSheet = dialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
             val behavior: BottomSheetBehavior<*> = BottomSheetBehavior.from(bottomSheet!!)
+//            behavior.isHideable = false
             behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            behavior.peekHeight = Resources.getSystem().displayMetrics.heightPixels
         }
-        return dialog
-
     }
 }
