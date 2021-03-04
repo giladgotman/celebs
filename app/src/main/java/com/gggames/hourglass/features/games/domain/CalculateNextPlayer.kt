fb1d0464c2c0f723a0e26b19464c8f66e854881a
giladgotman <gg@idagio.com>
2021-03-04 11:01:25 +0100

    handle case where a team has an empty list of players

--- app/src/main/java/com/gggames/hourglass/features/games/domain/CalculateNextPlayer.kt
+++ app/src/main/java/com/gggames/hourglass/features/games/domain/CalculateNextPlayer.kt
@@ -14,14 +14,15 @@ class CalculateNextPlayer @Inject constructor(){
             }
         } else {
             val lastTeamIdx = teams.indexOfFirst { it.name == lastTeamName }
-            val nextTeam = teams[(lastTeamIdx + 1) % teams.size]
-            if (nextTeam.players.isNotEmpty()) {
+
+            for (i in 1..teams.size) {
+                val nextTeam = teams[(lastTeamIdx + i) % teams.size]
+                if (nextTeam.players.isEmpty()) continue
                 val lastPlayerIdx = nextTeam.players.indexOfFirst { it.id == nextTeam.lastPlayerId }
                 val nextPlayer = nextTeam.players[(lastPlayerIdx + 1) % nextTeam.players.size]
-                nextPlayer
-            } else {
-                null
+                return nextPlayer
             }
+            return null
         }
     }
 }
