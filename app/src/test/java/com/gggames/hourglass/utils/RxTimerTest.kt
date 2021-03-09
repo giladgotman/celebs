53eceb8bffc6c3f2759eab0716fcc9588ded98ee
giladgotman <gg@idagio.com>
2021-03-09 15:09:14 +0100

    add more RxTimer tests

--- app/src/test/java/com/gggames/hourglass/utils/RxTimerTest.kt
+++ app/src/test/java/com/gggames/hourglass/utils/RxTimerTest.kt
@@ -1,6 +1,6 @@
 package com.gggames.hourglass.utils
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.idagio.app.core.utils.rx.scheduler.TrampolineSchedulerProvider
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
@@ -17,11 +17,48 @@ class RxTimerTest{
         }
     }
     @Test
-    fun `Good weather flow`() {
+    fun `Good weather flow - start`() {
         tested.time = 1000
         val observer = tested.observe().test()
         tested.start()
         timerObservable.onNext(0)
-        Truth.assertThat(observer.values().last()).isEqualTo(TimerEvent.TimerEnd)
+        assertThat(observer.values().last()).isEqualTo(TimerEvent.TimerEnd)
+    }
+
+
+    @Test
+    fun `Can do start stop start`() {
+        tested.time = 2000
+        val observer = tested.observe().test()
+        tested.start()
+        timerObservable.onNext(0)
+        assertThat(observer.values().first()).isEqualTo(TimerEvent.UpdatedTime(1000))
+
+        tested.stop()
+
+        assertThat(tested.time).isEqualTo(1000)
+        observer.values().clear()
+        tested.start()
+        timerObservable.onNext(0)
+        assertThat(observer.values().first()).isEqualTo(TimerEvent.UpdatedTime(0))
+        assertThat(observer.values().last()).isEqualTo(TimerEvent.TimerEnd)
+    }
+
+    @Test
+    fun `Good weather flow - pause resume`() {
+        tested.time = 2000
+        val observer = tested.observe().test()
+        tested.start()
+        timerObservable.onNext(0)
+        assertThat(observer.values().first()).isEqualTo(TimerEvent.UpdatedTime(1000))
+        observer.values().clear()
+
+        tested.pause()
+        timerObservable.onNext(0)
+        observer.assertNoValues()
+
+        tested.resume()
+        timerObservable.onNext(0)
+        assertThat(observer.values().last()).isEqualTo(TimerEvent.TimerEnd)
     }
 }
