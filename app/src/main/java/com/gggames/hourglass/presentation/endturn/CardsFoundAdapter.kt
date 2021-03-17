b83c1def08d9174a3f6c5a3632c3060981d5ab12
giladgotman <gg@idagio.com>
2021-03-17 16:49:33 +0200

    don't use the videoPlayer and add a giftGame flag which is set to false

--- app/src/main/java/com/gggames/hourglass/presentation/endturn/CardsFoundAdapter.kt
+++ app/src/main/java/com/gggames/hourglass/presentation/endturn/CardsFoundAdapter.kt
@@ -10,8 +10,10 @@ import com.gggames.hourglass.model.Card
 import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.android.synthetic.main.end_turn_card_found_item.view.*
 
-class CardsFoundAdapter(val onClick: (card: Card, playerView: PlayerView, giftText: TextView) -> Unit,
-                        val onClose: (PlayerView) -> Unit) :
+class CardsFoundAdapter(
+    val onClick: ((card: Card, playerView: PlayerView, giftText: TextView) -> Unit?)?,
+    val onClose: ((PlayerView) -> Unit)?
+) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var dataSet: List<Card> = emptyList()
@@ -20,6 +22,7 @@ class CardsFoundAdapter(val onClick: (card: Card, playerView: PlayerView, giftTe
         dataSet = cards
         notifyDataSetChanged()
     }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -37,16 +40,16 @@ class CardsFoundAdapter(val onClick: (card: Card, playerView: PlayerView, giftTe
         fun bind(item: Card) {
             itemView.name.text = item.name
             itemView.setOnClickListener {
-                onClick(item, itemView.playerView, itemView.name)
+                onClick?.let { it(item, itemView.playerView, itemView.name) }
             }
             itemView.closeButton.setOnClickListener {
-                onClose(itemView.playerView)
+                onClose?.let { it(itemView.playerView) }
             }
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        return(holder as CardsFoundAdapter.CardsViewHolder).bind(dataSet[position])
+        return (holder as CardsFoundAdapter.CardsViewHolder).bind(dataSet[position])
     }
 }
 
