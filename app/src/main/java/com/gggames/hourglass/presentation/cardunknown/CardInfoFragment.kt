7409f924632b9d5655be8c32805f496c12e4cea8
giladgotman <gg@idagio.com>
2021-03-25 15:32:44 +0200

    add CardInfoFragment.kt

---
+++ app/src/main/java/com/gggames/hourglass/presentation/cardunknown/CardInfoFragment.kt
@@ -0,0 +1,31 @@
+package com.gggames.hourglass.presentation.cardunknown
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.gggames.hourglass.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_card_info.*
+
+class CardInfoFragment: BottomSheetDialogFragment() {
+
+    fun show(activity: AppCompatActivity) {
+        show(activity.supportFragmentManager, this.javaClass.simpleName)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_card_info, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        closeButton.setOnClickListener {
+            dismiss()
+        }
+    }
+}
